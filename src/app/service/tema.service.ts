ae7ddec685c839dec13f5657aa4d5f91d1df73e1
Yuri <yuri.rampazo@outlook.com>
2021-05-06 22:16:18 -0300

    added delete-tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -37,6 +37,6 @@ export class TemaService {
   }
 
   deleteTema(id: number) {
-    return this.http.delete<Tema>(`http://localhost:8080/tema/${id}`,this.token)
+    return this.http.delete(`http://localhost:8080/tema/${id}`,this.token)
   }
 }
